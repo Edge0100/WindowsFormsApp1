de61e191385e57ae482249e1185a5a9488d45399
Edge0100 <ahrejl97@gmail.com>
2020-02-13 16:20:21 +0300

    Путь к документу, выход при тусклой красном цвете на зелёном квадрате

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -83,10 +83,14 @@ namespace WindowsFormsApp1
                         break;
                     }
             }
-            if (getTileInfo(cube.getMapX(), cube.getMapY()) == Status.Dead) this.Close();
-            graphics.Clear(Color.White);
-            DrawMap();
-            cube.DrawCube(graphics);
+            var a = getTileInfo(cube.getMapX(), cube.getMapY());
+            if (a == Status.Dead||(cube.redSideOnBottom()&&a==Status.Winner)) this.Close();
+            else
+            {
+                graphics.Clear(Color.White);
+                DrawMap();
+                cube.DrawCube(graphics);
+            }
 
         }
 
@@ -116,7 +120,7 @@ namespace WindowsFormsApp1
 
             graphics = CreateGraphics();
 
-            mapData = File.ReadAllLines("C:/Users/admin/Desktop/WindowsFormsApp1/WindowsFormsApp1/Новый текстовый документ.txt");
+            mapData = File.ReadAllLines("./Новый текстовый документ.txt");
             mapGraphics = new Rectangle[mapData.Length][];
 
             for (int i=0; i<mapData.Length;i++)
@@ -389,6 +393,10 @@ namespace WindowsFormsApp1
                     }
             }
             
+        }
+        public bool redSideOnBottom()
+        {
+            return redSide == Sides.Bottom;
         }
 
 
